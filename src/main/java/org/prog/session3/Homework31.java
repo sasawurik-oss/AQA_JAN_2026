56ac3399d84d88019ccb0eee90a56ea2a508f916
Aleksandra <sasawurik@gmail.com>
2026-05-19 13:39:01 +0300

    homework3.1

---
+++ src/main/java/org/prog/session3/Homework31.java
@@ -0,0 +1,35 @@
+package org.prog.session3;
+
+import java.util.Random;
+
+public class Homework31 {
+
+    public static void main (String[] args) {
+        Random random = new Random();
+        int [] mysort = new int[100];
+        for (int i = 0; i < mysort.length; i++){
+            mysort[i] = random.nextInt(1000);
+            System.out.println(mysort[i]);
+        }
+        System.out.println("=========================");
+        boolean condition = true;
+
+        do {
+            condition = false;
+            for (int i = 0; i < mysort.length - 1; i++) {
+                if (mysort[i] > mysort[i + 1]) {
+                    int current = mysort[i];
+                    mysort[i] = mysort[i + 1];
+                    mysort[i + 1] = current;
+                    condition = true;
+                }
+
+            }
+        } while (condition);
+
+        for (int i = 0; i < mysort.length; i++){
+            System.out.print(mysort[i]);
+        }
+    }
+}
+
