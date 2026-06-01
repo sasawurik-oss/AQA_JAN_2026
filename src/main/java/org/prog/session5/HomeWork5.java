dc15b149c9140ad8af365fb760c76504fb85608a
Aleksandra <sasawurik@gmail.com>
2026-06-02 00:16:35 +0300

    homewor5

--- src/main/java/org/prog/session5/HomeWork5.java
+++ src/main/java/org/prog/session5/HomeWork5.java
@@ -1,22 +1,27 @@
-package org.prog.session5;
 
+package org.prog.session5;
 public class HomeWork5 {
-    public String color;
-    public int mileage;
-    public String plateNumber;
-    public String owner;
+
 
     public static void main(String[] args){
-        HomeWork5 car = new HomeWork5();
-        car.color = "pink";
-        car.mileage = 110;
-        car.owner = "Alexandra";
-        car.plateNumber = "AL-987";
+        Car cars = new Car();
+        cars.color = "pink";
+        cars.mileage = 110;
+        cars.owner = "Alexandra";
+        cars.plateNumber = "AL-987";
 
-        car.goTo("Riga", 50);
+        cars.goTo("Riga", 50);
+    }
+     class Car {
+
+        public String color;
+        public int mileage;
+        public String plateNumber;
+        public String owner;
     }
 
-    public void goTo(String destination, int speed) {
+
+     public void goTo(String destination, int speed) {
         System.out.println("before trip miles: " + mileage);
         if ("pink".equals(color)) {
             System.out.println("Pink cars are cute:)");
