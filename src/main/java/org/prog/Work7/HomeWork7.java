deb09843a75f2208d3b68d01ad42cb672f93a52b
Aleksandra <sasawurik@gmail.com>
2026-06-15 22:59:16 +0300

    homewor71

--- src/main/java/org/prog/Work7/HomeWork7.java
+++ src/main/java/org/prog/Work7/HomeWork7.java
@@ -1,10 +1,16 @@
 package org.prog.Work7;
 
 public class HomeWork7 {
+    private static String Apple;
+
     public static void main(String[] args){
         Android android = new Android();
         Apple apple = new Apple();
+        Apple apple1 = new Apple();
 
+
+        apple1.color = "Black";
+        apple1.model = "11 Pro";
 
         apple.color = "Red";
         apple.model = "11 Pro";
@@ -21,6 +27,11 @@ public class HomeWork7 {
         System.out.println(apple.equals(android));
         System.out.println(apple.hashCode());
         System.out.println( apple.color +  " Apple " + apple.model  );
+        work(apple);
+        System.out.println("===============================");
+        System.out.println(apple1.equals(apple));
+        System.out.println(apple1.hashCode());
+        System.out.println( apple1.color +  " Apple " + apple1.model  );
         work(apple);
 
 
