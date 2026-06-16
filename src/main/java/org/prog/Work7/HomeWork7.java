96e63c60efc1a2cff0767599f3d96ad99b582e15
Aleksandra <sasawurik@gmail.com>
2026-06-16 20:15:17 +0300

    HomeWork7

--- src/main/java/org/prog/Work7/HomeWork7.java
+++ src/main/java/org/prog/Work7/HomeWork7.java
@@ -6,11 +6,12 @@ public class HomeWork7 {
     public static void main(String[] args){
         Android android = new Android();
         Apple apple = new Apple();
+        Apple apple1 = new Apple();
 
 
 
-        apple.color = "Red";
-        apple.model = "11 Pro";
+        apple1.color = "Red";
+        apple1.model = "11 Pro";
 
         apple.color = "Red";
         apple.model = "11 Pro";
@@ -18,20 +19,20 @@ public class HomeWork7 {
         android.color = "Black";
        android.model = "12";
 
-        System.out.println(android.equals(apple));
+
         System.out.println(android.hashCode());
         System.out.println( android.color +  " Android " + android.model  );
         work(android);
         System.out.println("===============================");
         System.out.println("===============================");
-        System.out.println(apple.equals(android));
+        System.out.println(apple.color.equals(apple1.color));
         System.out.println(apple.hashCode());
         System.out.println( apple.color +  " Apple " + apple.model  );
         work(apple);
         System.out.println("===============================");
-        System.out.println(apple.equals(apple));
-        System.out.println(apple.hashCode());
-        System.out.println( apple.color +  " Apple " + apple.model  );
+        System.out.println(apple1.model.equals(apple.model));
+        System.out.println(apple1.hashCode());
+        System.out.println( apple1.color +  " Apple " + apple1.model  );
         work(apple);
 
 
